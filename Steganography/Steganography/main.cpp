bb58c1019851bd36135d3f8b90ffaef22b419ac7
alimelon <alimelon97@gmail.com>
2021-03-22 01:21:02 +0900

    프로젝트 생성 및 .gitignore 추가

---
+++ Steganography/Steganography/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Steganography
+//
+//  Created by A on 2021/03/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
