5b5302c38bdfff7fdb2a323e825e9bc9f3ab8ddf
alimelon <alimelon97@gmail.com>
2021-03-23 18:51:02 +0900

    bmp 파일 확인

--- Steganography/Steganography/main.cpp
+++ Steganography/Steganography/main.cpp
@@ -23,13 +23,13 @@ int main(int argc, const char * argv[]) {
     
     fseek(fd, 0, SEEK_END);
     long lSize = ftell(fd);
-    cout << lSize << endl;
     
     unsigned char *buffer = (unsigned char*)malloc(lSize + 1);
     memset(buffer, 0, lSize + 1);
     
-    unsigned char bfType[2];
-    unsigned char bfSize[2];
+    char bfType[2];
+    unsigned char bfSize[4];
+    unsigned char startOffset[4];
     
     fseek(fd, 0, SEEK_SET);
     fread(bfType, 2, 1, fd);
@@ -38,20 +38,37 @@ int main(int argc, const char * argv[]) {
     
     cout << bfType << endl;
     cout << bfSize << endl;
-//
-//    printf("%x ", bfType[0]);
-//    printf("%x ", bfSize[0]);
     
-//    for (int i = 0; bfType[i]!='\0'; i++) {
-//        //sizeof로 하면 배열의 크기인 100자까지 나오므로 null값인 \0까지 for문 반복
-//        printf("%x ", bfType[i]);
-//    }
+    fseek(fd, 10, SEEK_SET);
+    fread(buffer, 4, 1, fd);
+    cout << "==" << endl;
+    //cout << buffer << endl;
     
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < 4; i++) {
         //sizeof로 하면 배열의 크기인 100자까지 나오므로 null값인 \0까지 for문 반복
         printf("%d ", i);
-        printf("%x ", bfSize[i]);
+        printf("%x ", buffer[i]);
     }
+    
+    char bm[] = "BM";
+    
+    if ( strcmp(bm, bfType) == 0 ) {
+        cout << "bmp 파일이 맞아요." << endl;
+    }
+    //
+    //    printf("%x ", bfType[0]);
+    //    printf("%x ", bfSize[0]);
+    
+    //    for (int i = 0; bfType[i]!='\0'; i++) {
+    //        //sizeof로 하면 배열의 크기인 100자까지 나오므로 null값인 \0까지 for문 반복
+    //        printf("%x ", bfType[i]);
+    //    }
+    
+    //    for (int i = 0; i < 2; i++) {
+    //        //sizeof로 하면 배열의 크기인 100자까지 나오므로 null값인 \0까지 for문 반복
+    //        printf("%d ", i);
+    //        printf("%x ", bfSize[i]);
+    //    }
     
     
     
