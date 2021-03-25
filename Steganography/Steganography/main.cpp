5cd55d195c201b4f48b0fb82dad13345fbc2ade8
alimelon <alimelon97@gmail.com>
2021-03-25 19:09:37 +0900

    inputmessage 비트 변환

--- Steganography/Steganography/main.cpp
+++ Steganography/Steganography/main.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <bitset>
 
 using namespace std;
 
@@ -34,27 +35,59 @@ int main(int argc, const char * argv[]) {
     fseek(fd, 0, SEEK_SET);
     fread(bfType, 2, 1, fd);
     fseek(fd, 2, SEEK_SET);
-    fread(bfSize, 2, 1, fd);
+    fread(bfSize, 4, 1, fd);
     
     cout << bfType << endl;
-    cout << bfSize << endl;
-    
-    fseek(fd, 10, SEEK_SET);
-    fread(buffer, 4, 1, fd);
-    cout << "==" << endl;
-    //cout << buffer << endl;
+    cout << "bfSize" << endl;
     
     for (int i = 0; i < 4; i++) {
-        //sizeof로 하면 배열의 크기인 100자까지 나오므로 null값인 \0까지 for문 반복
         printf("%d ", i);
-        printf("%x ", buffer[i]);
+        printf("%x ", bfSize[i]);
     }
+    
+//    int size = static_cast<int>(bfSize);
     
     char bm[] = "BM";
-    
-    if ( strcmp(bm, bfType) == 0 ) {
-        cout << "bmp 파일이 맞아요." << endl;
+
+    if ( strcmp(bm, bfType) != 0 ) {
+        cout << "bmp 파일이 아닙니다." << endl;
+        exit(1);
     }
+    
+    fseek(fd, 10, SEEK_SET);
+    fread(startOffset, 4, 1, fd);
+    
+    int num = 0;
+    num = static_cast<int>(startOffset[0]);
+    printf("hi %d", num);
+    
+    fseek(fd, num, SEEK_SET);
+    fread(buffer, 100, 1, fd);
+  
+    cout << "" << endl;
+    
+    for (int i = 0; i < 100; i++) {
+        //sizeof로 하면 배열의 크기인 100자까지 나오므로 null값인 \0까지 for문 반복
+        printf("%d: ", i);
+        printf("%x ", buffer[i]);
+        printf("%c ", buffer[i]);
+    }
+    
+    int arrIndex = 0;
+    
+    char inputText[] = "hi i'm seongJu";
+    for (int i = 0; inputText[i] != '\0'; i++) {
+        cout << i ;
+        cout << inputText[i] ;
+        cout << bitset<8>(inputText[i]) << endl;
+        
+        for (int j = 6; j<8; j++) {
+            printf("arrIndex: %d", arrIndex);
+            
+        }
+    }
+    
+    
     //
     //    printf("%x ", bfType[0]);
     //    printf("%x ", bfSize[0]);
@@ -107,3 +140,4 @@ int main(int argc, const char * argv[]) {
     //    readFile.close();
     return 0;
 }
+
